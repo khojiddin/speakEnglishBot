dc5bed4bff870b0a7bab2d725bdbb1f4270a4461
KHOJIDDIN <88823011+KHOJIDDIN@users.noreply.github.com>
2022-06-08 10:20:06 +0800

    commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from EnglishToEnglish import getWordDefinition
 from googletrans import Translator
 
+
 translator = Translator()
 
 BOT_API_TOKEN = os.getenv('Bot_Api')
@@ -13,6 +14,23 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_API_TOKEN)
 dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 @dp.message_handler(commands=['start'])
@@ -59,4 +77,11 @@ async def translate(message: types.Message):
         await message.reply("Something went wrong or we cannot translate from this language, please Try again")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,)
